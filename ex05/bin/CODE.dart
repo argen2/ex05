032b00bf0b326a86741b5813fed46f990442bdda
argen2 <chiefmulot@gmail.com>
2013-10-05 09:52:49 -0400

    derniere sauvegarde

--- ex05/bin/CODE.dart
+++ ex05/bin/CODE.dart
@@ -10,8 +10,8 @@ ex05_1(){
     s = '${s}${ch} ';
   }
   var alpahabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];                 
-
   print(s);
+  
   print(mot.split('') is List);
  
   
